85df6b5839ed835feb044ade97afcb86a9830cd6
Wendy <fenniew713jb@gmail.com>
2022-01-01 12:43:16 +0800

    定義網站存取路徑規則

---
+++ topic/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', TopicList.as_view(), name='topic_list'),
+    path('new/', TopicNew.as_view(), name='topic_new'), 
+    path('<int:pk>/', TopicView.as_view(), name='topic_view'),
+]
