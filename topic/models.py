261f5a91c21202370bfa9269752a4ee14ca9cfe5
Wendy <fenniew713jb@gmail.com>
2022-01-01 12:37:51 +0800

    create_topic_models

--- topic/models.py
+++ topic/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# 討論主題
+class Topic(models.Model):
+    subject = models.CharField('討論主題', max_length=255)
+    content = models.TextField('內文')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField('建立時間', auto_now_add=True)
+    replied = models.DateTimeField('回覆時間', null=True, blank=True)
+
+    def __str__(self):
+        return "{}: {}".format(self.author, self.subject)
