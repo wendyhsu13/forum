246d0756cb2d49e853433be3168fcb65ee0ec9fa
Wendy <fenniew713jb@gmail.com>
2022-01-01 12:44:10 +0800

    定義處理視圖

--- topic/views.py
+++ topic/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.views.generic import *
+from django.urls import reverse
+from .models import *
 
-# Create your views here.
+# 討論主題列表
+class TopicList(ListView):
+    model = Topic
+    ordering = ['-created']
+    paginate_by = 20        # 每頁主題數
+
+# 新增討論主題
+class TopicNew(CreateView):
+    model = Topic
+    fields = ['subject', 'content']
+
+    def get_success_url(self):
+        return reverse('topic_list')
+
+    def form_valid(self, form):
+        # 自動將目前使用者填入討論主題的作者欄
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# 檢視討論主題
+class TopicView(DetailView):
+    model = Topic
