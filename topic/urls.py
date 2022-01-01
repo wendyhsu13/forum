d98126ac2a6b5c9800046ab7957d73e1f22ab7e0
Wendy <fenniew713jb@gmail.com>
2022-01-01 13:05:03 +0800

    刪除討論主題

--- topic/urls.py
+++ topic/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('new/', TopicNew.as_view(), name='topic_new'), 
     path('<int:pk>/', TopicView.as_view(), name='topic_view'),
     path('<int:tid>/reply/', TopicReply.as_view(), name='topic_reply'),
+    path('<int:pk>/delete/', TopicDelete.as_view(), name='topic_delete'),
+    path('reply/<int:pk>/delete/', ReplyDelete.as_view(), name='reply_delete'),
 ]
--- topic/views.py
+++ topic/views.py
@@ -2,6 +2,7 @@ from django.views.generic import *
 from django.urls import reverse
 from .models import *
 from datetime import datetime
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 # 討論主題列表
 class TopicList(ListView):
@@ -48,3 +49,22 @@ class TopicReply(CreateView):
 
     def get_success_url(self):
         return reverse('topic_view', args=[self.kwargs['tid']])
+
+# 刪除討論主題
+class TopicDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'topic.delete_topic'
+    model = Topic
+    template_name = 'confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse('topic_list')
+
+# 刪除討論回覆
+class ReplyDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = 'topic.delete_reply'
+    model = Reply
+    template_name = 'confirm_delete.html'
+
+    def get_success_url(self):
+        reply = self.get_object()   # 取得欲刪除的那筆紀錄
+        return reverse('topic_view', args=[reply.topic.id])
