28de508bfbbb353a06aeb65f0a0e24eac24f670b
Wendy <fenniew713jb@gmail.com>
2022-01-01 13:15:06 +0800

    ForeignKey

--- topic/views.py
+++ topic/views.py
@@ -27,11 +27,11 @@ class TopicNew(CreateView):
 class TopicView(DetailView):
     model = Topic
 
-    def get_context_data(self, **kwargs):
-        # 取得回覆資料傳給頁面範本處理
-        ctx = super().get_context_data(**kwargs)
-        ctx['reply_list'] = Reply.objects.filter(topic=self.object)
-        return ctx
+    def get_object(self):
+        topic = super().get_object()    # 取得欲查看的討論主題
+        topic.hits += 1     # 等同 topic.hits = topic.hits + 1
+        topic.save()
+        return topic
 
     def get_object(self):
         topic = super().get_object()    # 取得欲查看的討論主題
